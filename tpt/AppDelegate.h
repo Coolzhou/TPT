05c4dcaff010f0936e17d1665cc1c3db48c047a8
lxz <xeaje0000@126.com>
2016-07-19 14:11:54 +0800

    宝宝手环 项目起始

---
+++ tpt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tpt
+//
+//  Created by apple on 16/7/19.
+//  Copyright © 2016年 MDJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
